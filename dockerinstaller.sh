9ef3677a91c8b09a1329b70f8266368a2b4f3a72
passinate <lokendrabhat6@gmail.com>
2024-01-10 16:11:41 +0545

    Updated dockerinstaller

--- dockerinstaller.sh
+++ dockerinstaller.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 
 sudo apt-get install -y ca-certificates curl gnupg
 
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo install -y -m 0755 -d /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -22,7 +22,8 @@ sudo apt-get update
 # Installing docker package
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# sudo usermod -aG docker $USER
+# To add current user into docker group
+sudo usermod -aG docker $USER
 
 # Verify docker running in our system
 sudo docker run hello-world
