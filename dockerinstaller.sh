e0ee02d616ad5e11d1cbddcb3817333319c29d2f
passinate <lokendrabhat6@gmail.com>
2024-01-10 15:44:34 +0545

    scriptfile created

---
+++ dockerinstaller.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+sudo apt-get update
+
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package again
+sudo apt-get update
+
+# Installing docker package
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# sudo usermod -aG docker $USER
+
+# Verify docker running in our system
+sudo docker run hello-world
+
+
+# Reference: official docker documentation site https://docs.docker.com/engine/install/ubuntu/
+
+
